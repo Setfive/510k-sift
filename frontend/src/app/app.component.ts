c910bb4674bef487c73d588aef1a6d3822691a99
Ashish Datta <ashish@setfive.com>
2023-08-02 21:45:21 -0400

    making navbar responsive

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -9,6 +9,7 @@ import {Router} from "@angular/router";
 export class AppComponent {
   title = '510(k) Sift';
   activeNav = 'home';
+  isMenuCollapsed = true;
   public constructor(
     private router: Router,
   ) {}
